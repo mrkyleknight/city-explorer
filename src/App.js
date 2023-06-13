21c7a84d71ef0911d7f6a6330e96b6619f2a8d14
Kyle Knight <knightkyle@outlook.com>
2023-06-13 02:21:33 -0700

    Upload Lab-6

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      city: '',
+      locationData: {},
+      error: false,
+      errorMsg: ''
+    };
+  }
+
+  handleCityInput = (event) => {
+    this.setState({
+      city: event.target.value
+    });
+  };
+
+  handleGetCityInfo = async (event) => {
+    event.preventDefault();
+
+    try {
+      let url = `https://us1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_KEY_API}&q=${this.state.city}&format=json`;
+      let response = await axios.get(url);
+
+      const { display_name, lat, lon } = response.data[0];
+
+      this.setState({
+        locationData: {
+          display_name,
+          lat,
+          lon
+        },
+        error: false,
+        errorMsg: ''
+      });
+    } catch (error) {
+      this.setState({
+        error: true,
+        errorMsg: error.response.data.error
+      });
+    }
+  };
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.handleGetCityInfo}>
+          <label htmlFor=""> Enter a City Name:
+            <input type="text" onInput={this.handleCityInput} />
+          </label>
+          <button type="submit">Explore!</button>
+        </form>
+
+        {
+          this.state.error
+            ? <p>{this.state.errorMsg}</p>
+            : (
+              <div>
+                <p>Location: {this.state.locationData.display_name}</p>
+                <p>Latitude: {this.state.locationData.lat}</p>
+                <p>Longitude: {this.state.locationData.lon}</p>
+              </div>
+            )
+        }
+      </>
+    );
+  }
 }
 
 export default App;
